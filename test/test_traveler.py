7767be5e7af37ac9d6b7148f064ca47d8a208f10
Atomute <73423300+Atomute@users.noreply.github.com>
2023-01-18 00:27:26 +0700

    add test and indexer

--- test/test_traveler.py
+++ test/test_traveler.py
@@ -1,11 +1,17 @@
 import unittest
 import sys
-sys.path.insert(1,"/work/PythonSearchEngine/")
+sys.path.insert(1,"./")
 from spider_webTraveler import webTraveler
 
 class test_find_links(unittest.TestCase):
+    def setUp(self):
+        self.traveler = webTraveler()
+
     def test_normal(self):
-        pass
+        input = "<a>link1</a>    <a>link2</a>"
+        tester = self.traveler.find_links(input)
+
+        self.assertIsInstance(tester,list)
 
     def test_numbersign(self):
         pass
@@ -13,3 +19,5 @@ class test_find_links(unittest.TestCase):
     def test_notinroot(self):
         pass
 
+if __name__ == '__main__':
+    unittest.main()  
