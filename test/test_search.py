3187745a28e89d6c8c338a1feda37a117fd42331
Atomute <73423300+Atomute@users.noreply.github.com>
2023-03-19 13:01:47 +0700

    add more test and more UI

--- test/test_search.py
+++ test/test_search.py
@@ -4,18 +4,20 @@ import sys
 sys.path.insert(1,"./")
 from spider.spider import spider
 from database.DB_sqlite3 import DB
-from search.main import searcher
+from search.searcher import searcher
 
-class test_searcher(unittest.TestCase):
+class test_scoreWord(unittest.TestCase):
     def setUp(self):
         self.searcher = searcher()
 
-    def test_type(self):
-        # return type should be list
-        test_query = "search query"
-        tester = self.searcher.search(test_query)
+    @patch('database.DB_sqlite3.DB.connect.cursor.fetchall')
+    def test_normal(self,mock_cursor):
+        mock_cursor.return_value = [(1,3,3.5),(2,2,0),(3,1,5)]
+        tester = self.searcher.scoreWord([1,2,3])
 
-        self.assertIsInstance(tester,list)
+        self.assertEqual(tester,{})
+        
+
 
     # def test_normal(self):
     #     # return list should contain website ID integer
